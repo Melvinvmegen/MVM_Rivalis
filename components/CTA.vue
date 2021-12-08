90c029160b5bb6c7ddd36777ff474a96e8dfc837
Melvin Van Megen <melvin.vmegen@gmail.com>
2021-12-08 17:19:25 +0100

    UP (CTA) button component

---
+++ components/CTA.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .my-5(:class="{centered: centered}")
+    a.text-decoration-none.white--text(href='tel:+33788201727' data-cursor-hover data-cursor-mix-blend-mode="difference")
+      v-btn(color='primary' large='')
+        .white--text
+          | {{ text }}
+        v-icon.ml-2 fa-phone
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: null
+    },
+    centered: Boolean,
+    link: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style>
+.centered {
+  display: flex;
+  justify-content: center;
+}
+</style>
