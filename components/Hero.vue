a6388412b57a98c3adb4691765756f16af7a5693
Melvin Van Megen <melvin.vmegen@gmail.com>
2021-12-08 14:09:45 +0100

    UP (Hero) component with media query image

---
+++ components/Hero.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+.hero.section--light
+  v-container.col-lg-6.col-md-8.col-sm-10.col-12.pa-md-0
+    TextHeader(:title="title")
+    span(v-html="description")
+    CTA(v-if="ctaText && link" :className="true" color="accent" :text="ctaText" :link="link")
+  v-img.hero-image(:src='require(`~/assets/Michel van Megen.png`)' v-if='this.$vuetify.breakpoint.smAndUp')
+  v-img.hero-image(:src='require(`~/assets/Michel van Megen Mini.png`)' v-else)
+
+</template>
+
+<script>
+import CTA from '~/components/CTA'
+import TextHeader from '~/components/TextHeader'
+
+export default {
+  components: {
+      CTA,
+      TextHeader
+  },
+  data () {
+    return {
+      title: 'COACHING, CONSEIL ET ACCOMPAGNEMENT DES DIRIGEANTS D’ENTREPRISES',
+      description: 'L’art de la réussite consiste à bien savoir s’entourer…',
+      ctaText: 'Contactez-moi',
+      link: 'contact'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 2.5rem
+  }
+  
+  .hero {
+    position: relative;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    background-color: transparent;
+    margin-top: -6rem;
+    min-height: calc(85vh - 6rem);
+  }
+
+  .hero-image {
+    width: 600px;
+    position: absolute;
+    bottom: 0;
+    right: 10%;
+    height: auto;
+    z-index: 4;
+    border-radius: 4%
+  }
+
+  @media (max-width: 1600px) { 
+    .hero-image {
+      right: 0
+    }
+  }
+
+  @media (max-width: 1400px) { 
+    .hero-image {
+      width: 400px
+    }
+  }
+
+  @media (max-width: 978px) { 
+    .hero-image {
+      width: 300px
+    }
+
+    h1 {
+      font-size: 2rem
+    }
+  }
+
+  @media (max-width: 450px) { 
+    .hero-image {
+      width: 250px
+    }
+
+    h1 {
+      font-size: 1.5rem
+    }
+  }
+
+</style>
